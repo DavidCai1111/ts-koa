00a491dd27c8bcaf4626c54e06fd0ce05d518753
DavidCai <376462191@qq.com>
2015-12-12 12:22:29 +0800

    basic req

---
+++ src/request.ts
@@ -0,0 +1,65 @@
+/// <reference path="../typings/node/node.d.ts" />
+'use strict'
+import {IncomingMessage} from 'http'
+const {parse} = require('parseurl')
+
+export class Request {
+  public request: Object
+  constructor(public req: IncomingMessage) {}
+
+  get(field: string): string {
+    const req = this.req
+    switch (field = field.toLowerCase()) {
+      case 'referer':
+      case 'referrer':
+        return req.headers.referrer || req.headers.referer || ''
+      default:
+        return req.headers[field] || ''
+    }
+  }
+
+  get headers(): Object {
+    return this.req.headers
+  }
+
+  get header(): Object {
+    return this.req.headers
+  }
+
+  get method(): string {
+    return this.req.method
+  }
+
+  set method(method: string) {
+    this.req.method = method
+  }
+
+  get length(): any {
+    const len = this.get('Content-Length')
+    if (len === '') return
+    return ~~len
+  }
+
+  get url(): string {
+    return this.req.url
+  }
+
+  set url(val: string) {
+    this.req.url = val
+  }
+
+  get originalUrl(): string {
+    return this.req.url
+  }
+
+  get path(): string {
+    return parse(this.req).pathname
+  }
+
+  set path(val: string) {
+    const url = parse(this.req)
+    if (url.pathname === val) return
+    url.pathname = val
+    url.path = null
+  }
+}
