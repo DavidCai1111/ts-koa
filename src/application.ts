4e2c8c20847f241891ef7b8416f37ac1f616f8cb
DavidCai <376462191@qq.com>
2015-12-17 10:34:58 +0800

    app

--- src/application.ts
+++ src/application.ts
@@ -1,22 +1,29 @@
 /// <reference path="../typings/node/node.d.ts" />
 /// <reference path="../typings/statuses/statuses.d.ts" />
+/// <reference path="../typings/debug/debug.d.ts"/>
 'use strict'
 import {EventEmitter} from 'events'
 import * as http from 'http'
+import * as stream from 'stream'
 import {empty} from 'statuses'
 import {compose} from './utils/compose'
 import {Context} from './context'
 import {Request} from './request'
 import {Response} from './response'
+import {isJSON} from './utils/isJSON'
 
 export class Koa extends EventEmitter {
   private middlewares: Array<Function>
+  private subdomainOffset: number
+  private proxy: Boolean
   public server: http.Server
   public env: string
 
   constructor() {
     super()
     this.middlewares = []
+    this.subdomainOffset = 2
+    this.proxy = false
     this.env = process.env.NODE_ENV || 'development'
   }
 
@@ -47,13 +54,48 @@ export class Koa extends EventEmitter {
 
   respond(ctx: Context): void {
     const res = ctx.res
-    const statusCode = ctx.res.statusCode
-    if (empty[statusCode]) {
-      ctx.body = null
-      res.end()
+    const code = ctx.res.statusCode
+    let body = ctx.body
+
+    if (empty[code]) {
+      body = null
+      return res.end()
     }
 
-    ctx.body = JSON.stringify(ctx.body)
-    res.end(ctx.body)
+    if (ctx.request.method === 'HEAD') {
+      if (isJSON(body)) ctx.response.length = Buffer.byteLength(JSON.stringify(body))
+      return res.end()
+    }
+
+    if (body === null) {
+      ctx.response.type = 'text'
+      ctx.response.length = Buffer.byteLength(String(code))
+      return res.end(body)
+    }
+
+    if (Buffer.isBuffer(body)) return res.end(body)
+    if (typeof body === 'string') return res.end(body)
+
+    body = JSON.stringify(body)
+    ctx.response.length = Buffer.byteLength(String(code))
+    res.end(body)
+  }
+
+  toJSON(): Object {
+    return {
+      subdomainOffset: this.subdomainOffset,
+      proxy: this.proxy,
+      env: this.env
+    }
+  }
+
+  inspect(): Object {
+    return this.toJSON()
+  }
+
+  onerror(err: any): void {
+    if (err.status === 404 || err.expose) return
+    const message: string = err.stack || err.toString()
+    console.error(`\n${message.replace(/^/gm, ' ')}`)
   }
 }
